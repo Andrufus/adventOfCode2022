0fdc9c4001dbb3b381467627ea4931215476e78b
Pierre-Emmanuel Vilcot <pevilcot@gmail.com>
2022-12-03 17:07:56 +0100

    Day 3 part two

--- src/main/java/days/Day3.java
+++ src/main/java/days/Day3.java
@@ -1,11 +1,15 @@
 package days;
 
 import com.google.common.base.Splitter;
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Streams;
 
 import java.util.List;
 import java.util.function.Function;
+import java.util.stream.Stream;
 
-public class Day3 extends Day<List<List<String>>> {
+public class Day3 extends Day<List<String>> {
 
     private final List<String> letters = List.of(
             "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
@@ -13,10 +17,8 @@ public class Day3 extends Day<List<List<String>>> {
 
 
     @Override
-    Function<String, List<List<String>>> getMappingFunction() {
-        return l -> Splitter.fixedLength(l.length() / 2).splitToStream(l)
-                .map(s -> Splitter.fixedLength(1).splitToList(s))
-                .toList();
+    Function<String, List<String>> getMappingFunction() {
+        return l ->  Splitter.fixedLength(1).splitToList(l);
     }
 
     @Override
@@ -26,21 +28,34 @@ public class Day3 extends Day<List<List<String>>> {
 
     @Override
     public String getResultPartOne() {
-        Integer total = inputSupplier.get()
-                .map(rucksacks -> {
-                    var compartment1 = rucksacks.get(0);
-                    var compartment2 = rucksacks.get(1);
+        Stream<String> total = inputSupplier.get()
+                .map(rucksack -> Lists.partition(rucksack, rucksack.size() / 2))
+                .map(splitRucksack -> {
+                    var compartment1 = splitRucksack.get(0);
+                    var compartment2 = splitRucksack.get(1);
                     return compartment1.stream().filter(compartment2::contains).toList().get(0);
-                })
-                .map(item -> letters.indexOf(item) + 1)
-                .reduce(Integer::sum)
-                .orElseThrow();
+                });
 
-        return "Sum of the priorities : " + total;
+        return "Sum of the priorities : " + mapToIntAndSum(total);
     }
 
     @Override
     public String getResultPartTwo() {
-        return null;
+        Stream<String> total = Streams.stream(Iterables.partition(inputSupplier.get().toList(), 3))
+                .map(groupedRucksacks -> {
+                    var sack1 = groupedRucksacks.get(0);
+                    var sack2 = groupedRucksacks.get(1);
+                    var sack3 = groupedRucksacks.get(2);
+                    return sack1.stream().filter(sack2::contains).filter(sack3::contains).toList().get(0);
+                });
+
+        return "Sum of the priorities : " + mapToIntAndSum(total);
+    }
+
+    private Integer mapToIntAndSum(Stream<String> letter) {
+        return letter
+                .map(item -> letters.indexOf(item) + 1)
+                .reduce(Integer::sum)
+                .orElseThrow();
     }
 }
